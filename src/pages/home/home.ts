19a1575ee31f67f54865906da40fe78344c01e54
Matthew Peters <matt@mattsterp.com>
2017-08-29 13:18:13 -0400

    Added logoicon and stlying for homepage.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { DataProvider } from '../../providers/data/data';
 import { Storage } from '@ionic/storage';
-
 
 /**
  * Generated class for the HomePage page.
@@ -10,7 +9,6 @@ import { Storage } from '@ionic/storage';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -19,11 +17,44 @@ import { Storage } from '@ionic/storage';
 export class HomePage {
 
   games = [];
+  genre: any;
+  genreName: string = "Upcoming Games";
+  favorites = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _data: DataProvider, private storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _data: DataProvider, private storage: Storage, public loading: LoadingController) {
 
-    this._data.getGames('5', 0)
-     .subscribe(res => this.games = res);
+    let loader = this.loading.create({
+      content: 'Getting Games..',
+    });
+
+    loader.present().then(() => {
+
+      this.storage.get('genre').then((val) => {
+        if (val) {
+          this.genre = val.id;
+          this.genreName = val.name;
+        } else {
+          this.genre = 5;
+          this.genreName = 'Shooter';
+          this.storage.set('genre', this.genre);
+        }
+
+        this._data.getGames(this.genre, 0)
+          .subscribe(res => this.games = res);
+      });
+
+      this.storage.get('favorites').then((val) => {
+        if (!val)
+          this.storage.set('favorites', this.favorites);
+        else
+          this.favorites = val;
+      });
+
+      setTimeout(() => {
+        loader.dismiss();
+      }, 1200);
+
+    });
 
   }
 
